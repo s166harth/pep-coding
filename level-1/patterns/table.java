f82edbe718597a257c688bf172db51501b442a23
s166harth <siddharthsagar2019@gmail.com>
2022-08-08 16:38:41 +0530

    patterns session 2

---
+++ level-1/patterns/table.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+"X"+i+"="+n*i);
+        }
+    }
+}
